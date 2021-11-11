ec4fb462688b8c12fe064fa771f41715d585999f
SlavikLLL <listopads091@gmail.com>
2021-11-11 20:24:35 +0200

    added modules

---
+++ index.js
@@ -0,0 +1,13 @@
+  const summa = (a, b) => {
+     const c = a + b;
+     return c;
+   };
+  const minus = (a, b) => {
+     const c = a - b;
+     return c;
+   };
+  const umnoj = (a, b) => {
+     const c = a * b;
+     return c;
+   };
+
